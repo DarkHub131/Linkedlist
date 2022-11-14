a12fe93dd5301d0c83fb4b963a78c55f9924bb7c
Nahid Hasan <117261871+DarkHub131@users.noreply.github.com>
2022-11-14 22:16:42 +0600

    Update Codes Archive.cpp

--- Codes Archive.cpp
+++ Codes Archive.cpp
@@ -24,3 +24,28 @@
         return maxi;
     }
 };
+2. Balanced Parentheses
+class Solution {
+public:
+    bool isValid(string expr) {
+        stack<char> temp;
+
+	for (int i = 0; i < expr.length(); i++) {
+		if (temp.empty()) {
+
+			temp.push(expr[i]);
+		}
+		else if ((temp.top() == '(' && expr[i] == ')')|| (temp.top() == '{' && expr[i] == '}')|| (temp.top() == '[' && expr[i] == ']'))
+                            {
+			temp.pop();
+		}
+		else {
+			temp.push(expr[i]);
+		}
+	}
+	if (temp.empty()) {	
+		return true;
+	}
+	return false ;
+    }
+};
